264f13e1dbd50253699c1c15e6bc6a9b43a3db35
Chaitanya Gupta <chaitanya.g@directi.com>
2012-05-02 10:54:31 +0530

    Initial Commit

---
+++ MOMPrinter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MOMPrinter
+//
+//  Created by Chaitanya Gupta on 02/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
