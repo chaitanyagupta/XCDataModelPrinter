6a91f1d8048f4bf2f97d1117b720f7e97ed2f976
Chaitanya Gupta <chaitanya.g@directi.com>
2012-05-12 13:59:06 +0530

    Move MOMPrinter code into a different class.

---
+++ src/MOMPrinter.h
@@ -0,0 +1,15 @@
+//
+//  MOMPrinter.h
+//  XCDataModelPrinter
+//
+//  Created by Chaitanya Gupta on 12/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOMPrinter : NSObject
+
+- (void)printPath:(NSString *)path;
+
+@end
