eb7d127186560e23d90e411c2bdd9f9377d27293
Chaitanya Gupta <chaitanya.g@directi.com>
2012-05-24 16:17:28 +0530

    Start using MOMCompiler.
    Now we take a .xcdatamodel file as input, compile it to a .mom file,
    and print its contents on screen.

--- src/main.m
+++ src/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "utils.h"
+#import "MOMCompiler.h"
 #import "MOMPrinter.h"
 
 int main(int argc, const char * argv[])
@@ -15,8 +17,18 @@ int main(int argc, const char * argv[])
   @autoreleasepool {
     if (argc == 2) {
       NSString *path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+      MOMCompiler *compiler = [[[MOMCompiler alloc] init] autorelease];
+      NSString *compiledPath = [compiler compilePath:path];
+      if (compiledPath == nil) {
+        return 2;
+      }
       MOMPrinter *printer = [[[MOMPrinter alloc] init] autorelease];
-      [printer printPath:path];
+      if (![printer printPath:compiledPath]) {
+        return 2;
+      }
+    } else {
+      NSPrintf(@"Usage: %@ path_to_xcdatamodel_file\n", [[NSProcessInfo processInfo] processName]);
+      return 1;
     }
   }
   return 0;
